9f8757cfcb94629039ceeca340caf601e96b3f9f
Sopio Makharashvili <smakharashvili@corp.europe-bet.com>
2020-02-18 18:16:51 +0400

    create component

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { HttpModule } from '@angular/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    BrowserModule, 
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
