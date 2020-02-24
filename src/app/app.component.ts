1a1ddb91860e95ceb8e22376f04d0636a10e8b92
Sopio Makharashvili <smakharashvili@corp.europe-bet.com>
2020-02-24 17:37:25 +0400

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles: [`
-    h3{
-      color: red;
-    }
-  `]
+  styleUrls: ['./app.component.css'],
+  // styles: [`
+  //   h3{
+  //     color: red;
+  //   }
+  // `]
 })
 export class AppComponent {
   username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
